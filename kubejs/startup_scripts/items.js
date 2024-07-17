29ae49c4953c7674c421c5c588811cb39049adf3
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2024-07-17 17:43:16 -0400

    Fixed Issue #42

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -2,6 +2,30 @@ ItemEvents.modification(event => {
 
     // Diet
 
+    // Add nutritious values to apple cider to make it contribute to its dietary food group
+    event.modify('farmersdelight:apple_cider', item => {
+        item.foodProperties = food => {
+            food.hunger(1.0)
+            food.saturation(1.2)
+        };
+    });
+
+    // Add nutritious values to hot cocoa to make it contribute to its dietary food group
+    event.modify('farmersdelight:hot_cocoa', item => {
+        item.foodProperties = food => {
+            food.hunger(1.0)
+            food.saturation(1.2)
+        };
+    });
+
+    // Add nutritious values to melon juice to make it contribute to its dietary food group
+    event.modify('farmersdelight:melon_juice', item => {
+        item.foodProperties = food => {
+            food.hunger(1.0)
+            food.saturation(1.2)
+        };
+    });
+
     // Add nutritious values to milk to make it contribute to its dietary food group
     event.modify('minecraft:milk_bucket', item => {
         item.foodProperties = food => {
