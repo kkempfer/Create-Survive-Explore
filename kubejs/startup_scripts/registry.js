08ea33b887c2af322353d26ba132ae590dc5b02d
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2024-07-19 13:57:47 -0400

    Fixed Issue #48

---
+++ kubejs/startup_scripts/registry.js
@@ -0,0 +1,23 @@
+StartupEvents.registry('item', event => {
+
+    // Delightful Creators
+
+    // Add incomplete pasta dish for sequenced assembly recipe
+    event.create('incomplete_pasta_dish')
+        .texture('delightfulcreators:item/incomplete_pasta_dish')
+        .food(food => {
+            food
+                .hunger(3)
+                .saturation(0.3)
+        });
+
+    // Add incomplete squid ink pasta dish for sequenced assembly recipe
+    event.create('incomplete_squid_ink_pasta')
+        .texture('delightfulcreators:item/incomplete_squid_ink_pasta')
+        .food(food => {
+            food
+                .hunger(3)
+                .saturation(0.3)
+        });
+
+});
