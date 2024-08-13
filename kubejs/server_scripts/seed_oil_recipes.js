b83124d452514205316a8d42aeaa5ac415c432d0
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2024-08-12 20:52:57 -0400

    Improved comments

--- kubejs/server_scripts/seed_oil_recipes.js
+++ kubejs/server_scripts/seed_oil_recipes.js
@@ -1,5 +1,8 @@
 ServerEvents.tags('item', event => {
 
+    // Create Crafts & Additions
+
+    // Compact seeds into seed oil
     const seedsItems = [
         'corndelight:corn_seeds',
         'supplementaries:flax_seeds'
