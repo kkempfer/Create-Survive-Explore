4f733464ded7b56be637ba6a6a1d98fe6f1e008b
Kévin Kempfer <kevin.kempfer@hotmail.fr>
2024-08-12 20:53:34 -0400

    Added steel cogwheels. Relates to issue #111

---
+++ kubejs/server_scripts/steel_cogwheels.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('item', event => {
+
+    // Create: Extended Cogwheels Fabric
+
+    // Make steel cogwheels from cast iron
+    event.add('c:steel_ingots', 'createdeco:cast_iron_ingot');
+
+});
